8eb9315c6a3068bce7ab64a598911d5c76739729
marceloglacial <glacial@gmail.com>
2024-08-16 15:25:45 -0400

    add partners

--- src/api/partner/controllers/partner.js
+++ src/api/partner/controllers/partner.js
@@ -1,9 +1,27 @@
-'use strict';
+"use strict";
 
 /**
- * partner controller
+ *  partner controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::partner.partner');
+module.exports = createCoreController("api::partner.partner", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const partner = await strapi.entityService.findMany(
+      "api::partner.partner",
+      query
+    );
+
+    const sanitizedEntity = await this.sanitizeOutput(partner);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
--- src/api/partner/routes/partner.js
+++ src/api/partner/routes/partner.js
@@ -1,9 +1,52 @@
-'use strict';
+"use strict";
 
 /**
- * partner router
+ * partner router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
+const defaultRouter = createCoreRouter("api::partner.partner");
 
-module.exports = createCoreRouter('api::partner.partner');
+// function to add to or override default router methods
+const customRouter = (innerRouter, routeOveride = [], extraRoutes = []) => {
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes;
+
+      const newRoutes = routes.map((route) => {
+        let found = false;
+
+        routeOveride.forEach((overide) => {
+          if (
+            route.handler === overide.handler &&
+            route.method === overide.method
+          ) {
+            found = overide;
+          }
+        });
+
+        return found || route;
+      });
+
+      return newRoutes.concat(extraRoutes);
+    },
+  };
+};
+
+// Overide the default router with the custom router to use slug.
+const myOverideRoutes = [
+  {
+    method: "GET",
+    path: "/partners/:slug",
+    handler: "api::partner.partner.findOne",
+  },
+];
+
+const myExtraRoutes = [];
+
+module.exports = customRouter(defaultRouter, myOverideRoutes, myExtraRoutes);
